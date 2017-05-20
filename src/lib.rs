e065b67fe63a8e64df913f34286527ab82a9603e
Alex Maslakov <me@gildedhonour.com>
2017-05-20 12:05:19 +0700

    wip

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2017 Alex Maslakov, <http://gildedhonour.com>, <http://alexmaslakov.com>
+ *
+ * Licensed under the GPL, Version 3.0 (the "License");
+ * https://github.com/GildedHonour/frank_ai
+ *
+ */
+
+pub struct Neuron {
+
+}
+
+pub struct Layer {
+
+}
+
+pub struct NeuralNetwork {
+  layers: Vec<Layer>
+}
